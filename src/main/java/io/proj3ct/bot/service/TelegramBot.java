899debcab25c4c398857982fc57dc2f4c5a04a10
korall79 <koralll79@yandex.ru>
2023-05-12 16:39:39 +0400

    added log

---
+++ src/main/java/io/proj3ct/bot/service/TelegramBot.java
@@ -0,0 +1,65 @@
+package io.proj3ct.bot.service;
+
+import io.proj3ct.bot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) { // при получении обновления
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default: sendMessage(chatId, "Sorry, command was not recognized");
+            }
+        }
+
+    }
+
+    private void startCommandReceived(long chatId, String name) { //Команда запуска получена
+        String answer = "Hi, " + name + ", nice to meet you!"; //отвечать
+        log.info("Replied to user "+name);
+
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) { //Отправить сообщение
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: \"+ e.getMessage()");
+        }
+
+    }
+}
---
+++ src/test/java/io/proj3ct/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
